5d6348d794e927685fe36119b5565a270a774ae5
albaparticle <albaparticle@gmail.com>
2025-05-12 00:36:15 +0800

    Customer Home Page done

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,3 +1,74 @@
+import { db } from "@/db/db"
+import { Product } from "@prisma/client"
+import { Button } from '@/components/ui/button';
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import { Suspense } from "react";
+
+function getMostPopularProducts() {
+    return db.product.findMany({
+        where: {
+            isAvailableForPurchase: true
+        },
+        orderBy: {
+            orders: {
+                _count: 'desc'
+            }
+        },
+        take: 6
+    })
+}
+
+function getNewestProducts() {
+    return db.product.findMany({
+        where: {
+            isAvailableForPurchase: true
+        },
+        orderBy: {
+            createdAt: 'desc'
+        },
+        take: 6
+    })
+}
+
 export default function HomePage() {
-    return <h1>Hi</h1>
+    return <main className="space-y-12">
+        <ProductGridSection productsFetcher={getMostPopularProducts} title="Most Popular" />
+        <ProductGridSection productsFetcher={getNewestProducts} title="Newest" />
+    </main>
+}
+
+function ProductGridSection({ productsFetcher, title }: { productsFetcher: () => Promise<Product[]>; title: string }) {
+    return (
+        <div className="space-y-4">
+            <div className="flex gap-4">
+                <h2 className="text-3xl font-bold">{title}</h2>
+                <Button variant="outline" asChild>
+                    <Link href="/products" className="space-x-2">
+                        <span>View All</span>
+                        <ArrowRight className="size-4" />
+                    </Link>
+                </Button>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <Suspense fallback={
+                    <>
+                        <ProductCardSkeleton />
+                        <ProductCardSkeleton />
+                        <ProductCardSkeleton />
+                    </>
+                }>
+                    <ProductSuspense productsFetcher={productsFetcher} />
+                </Suspense>
+            </div>
+        </div>
+    )
+}
+
+async function ProductSuspense({ productsFetcher, }: { productsFetcher: () => Promise<Product[]> }) {
+    return (await productsFetcher()).map(product => (
+        <ProductCard key={product.id} {...product} />
+    ))
+
 }
